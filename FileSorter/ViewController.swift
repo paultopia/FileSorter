ac353582c0e48454c15a8467343b2fbbc43f808e
Paul Gowder <paul.gowder@gmail.com>
2019-02-09 21:39:36 -0600

    I have a table view!

--- FileSorter/ViewController.swift
+++ FileSorter/ViewController.swift
@@ -10,6 +10,7 @@ import Cocoa
 
 class ViewController: NSViewController {
     var filesList: [URL] = []
+    @IBOutlet var tableView: NSTableView!
     @IBAction func pickFilePressed(_ sender: Any) {
         let dialog = NSOpenPanel()
         dialog.title = "choose file"
@@ -25,6 +26,7 @@ class ViewController: NSViewController {
             filesList.append(result)
             print(filesList)
             fileName.stringValue = result.path
+            tableView.reloadData()
             }
         } else {
             // User clicked on "Cancel"
@@ -35,7 +37,8 @@ class ViewController: NSViewController {
     @IBOutlet var fileName: NSTextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
 
@@ -46,5 +49,37 @@ class ViewController: NSViewController {
     }
 
 
+}
+
+extension ViewController: NSTableViewDataSource {
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return filesList.count
+    }
+    
+}
+
+extension ViewController: NSTableViewDelegate {
+    
+    fileprivate enum CellIdentifiers {
+        static let NameCell = "NameCellID"
+}
+  func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+    var text: String = ""
+    var cellIdentifier: String = ""
+    
+    let item = filesList[row]
+    
+    if tableColumn == tableView.tableColumns[0] {
+        text = item.path
+        cellIdentifier = CellIdentifiers.NameCell
+    }
+    
+    if let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: cellIdentifier), owner: nil) as? NSTableCellView {
+        cell.textField?.stringValue = text
+        return cell
+    }
+    return nil
+    }
 }
 
