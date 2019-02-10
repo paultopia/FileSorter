bc84bfb8b788a1c374876201494b776028e40094
Paul Gowder <paul.gowder@gmail.com>
2019-02-09 20:56:04 -0600

    first attempt to do something

--- FileSorter/ViewController.swift
+++ FileSorter/ViewController.swift
@@ -9,7 +9,29 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    @IBAction func pickFilePressed(_ sender: Any) {
+        let dialog = NSOpenPanel()
+        dialog.title = "choose file"
+        dialog.showsResizeIndicator = true;
+        dialog.showsHiddenFiles = true;
+        dialog.canChooseDirectories = false;
+        dialog.canCreateDirectories = false;
+        dialog.allowsMultipleSelection = false;
+        dialog.allowedFileTypes = ["txt", "pdf"];
+        
+        if (dialog.runModal() == NSApplication.ModalResponse.OK) {
+            let result = dialog.url // Pathname of the file
+            
+            if let path = result?.path {
+                fileName.stringValue = path
+            }
+        } else {
+            // User clicked on "Cancel"
+            return
+        }
+    }
+    
+    @IBOutlet var fileName: NSTextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
