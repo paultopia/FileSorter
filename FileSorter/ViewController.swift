e114ec0f4afa79e7c344aeffb0b769d7034468ad
Paul Gowder <paul.gowder@gmail.com>
2019-02-09 20:38:57 -0600

    Initial Commit

---
+++ FileSorter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FileSorter
+//
+//  Created by Paul Gowder on 2/9/19.
+//  Copyright © 2019 Paul Gowder. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
